9eba3c2dc4f3dbf02141acda82c9b0a9e1781b89
ID00011977 <72946116+ID00011977@users.noreply.github.com>
2021-04-04 21:50:41 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,46 @@
 const express = require('express')
 const app = express()
 
-
+const fs = require('fs')
 
 app.set('view engine', 'pug')
 
 app.use('/static', express.static('public'))
+app.use(express.urlencoded({ extended: false }))
 
 //local host:8000
 app.get('/', (req, res) => {
     res.render('home')
 })
+
+app.post('/create', (req, res) => {
+    const title = req.body.title
+    const description = req.body.description
+
+    if (title.trim() === '' && description.trim() === '') {
+        res.render('create', { error: true })
+    } else {
+        fs.readFile('./data/works.json', (err, data) => {
+            if (err) throw err
+
+            const works = JSON.parse(data)
+
+            works.push({
+                id: id(),
+                title: title,
+                description: description,
+            })
+            fs.writeFile('./data/works.json', JSON.stringify(works), err => {
+                if (err) throw err
+                res.render('create', { success: true })
+            })
+        })
+    }
+
+   
+})
+
+app.use(express.static('images'))
 
 app.get('/create', (req, res) => {
     res.render('create')
@@ -32,3 +62,9 @@ app.listen(8000, err => {
 
     console.log('Server is running on port 8000...')
 })
+
+ function id () {
+    return '_' + Math.random().toString(36).substr(2, 9);
+  }
+
+
